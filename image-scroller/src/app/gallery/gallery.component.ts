35cec9c6b876fd58f6c98b1278b077ed3e6d512a
Fernando García <fernando@catalystdevs.com>
2019-07-21 17:53:29 +0200

    - Gallery component with Test

---
+++ image-scroller/src/app/gallery/gallery.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit} from '@angular/core';
+import {ImageService} from '../services/image.service';
+import {Subscription} from 'rxjs';
+import {Image} from "../models/image";
+
+@Component({
+  selector: 'app-gallery',
+  templateUrl: './gallery.component.html',
+  styleUrls: ['./gallery.component.scss'],
+})
+export class GalleryComponent implements OnInit {
+  private TIMES_TO_COPY = 11;
+  galleryImages: Image[];
+  imageSubscription: Subscription;
+  loadingImages: boolean;
+
+  constructor(public service: ImageService) { }
+
+  ngOnInit() {
+    this.loadingImages = true;
+    this.getImages();
+  }
+
+  getImages(): void {
+    this.imageSubscription = this.service
+      .getImages()
+      .subscribe(images => {
+        this.galleryImages = this.service.cloneSource(images, this.TIMES_TO_COPY);
+        this.loadingImages = false;
+      });
+  }
+
+  getDisplayImageUrl(id: number): string {
+    return `${this.service.DISPLAY_IMG_URL}${id}`;
+  }
+
+  ngOnDestroy(): void {
+    this.imageSubscription.unsubscribe();
+  }
+}
